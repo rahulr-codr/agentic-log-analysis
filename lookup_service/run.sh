078c07bf25898f53c57a26ab549dfb1990f22c4d
Rahul Radhakrishnan <59603500+rahulr-codr@users.noreply.github.com>
2025-03-15 16:00:22 -0700

    added nginx reverse proxy for quote service + otel

--- lookup_service/run.sh
+++ lookup_service/run.sh
@@ -1,6 +1,6 @@
 opentelemetry-instrument \
-    --traces_exporter console \
-    --metrics_exporter console \
-    --logs_exporter console \
+    --traces_exporter none \
+    --metrics_exporter none \
+    --logs_exporter none \
     --service_name lookup_service \
-    uvicorn lookup_service.main:app --host 0.0.0.0 --port 8000  --no-access-log --log-level info
+    uvicorn lookup_service.main:app --host 0.0.0.0 --port 8000 --no-access-log --log-level info
--- quote_api_service/run.sh
+++ quote_api_service/run.sh
@@ -1,7 +1,7 @@
 export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
 opentelemetry-instrument \
-    --traces_exporter oltp \
-    --metrics_exporter oltp \
-    --logs_exporter oltp \
+    --traces_exporter none \
+    --metrics_exporter none \
+    --logs_exporter none \
     --service_name quote-api-service \
-    uvicorn main:app --host 0.0.0.0 --port 8000  --no-access-log --log-level info
+    uvicorn main:app --host 0.0.0.0 --port 8000 --no-access-log --log-level info
